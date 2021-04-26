1fae1e78923da68953fb25423bb491c9d8b446c3
Casper <casp8249@edu.easj.dk>
2021-04-26 13:24:13 +0200

    Slider med javascript

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-// Get the modal
-var modal = document.getElementById("popupOpret");
 
-// Get the button that opens the modal
-var btn = document.getElementById("buttonOpret");
+ if (window.location.href.match('car-driving.html') != null) {
+    // Get the modal
+    var modal = document.getElementById("popupOpret");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    // Get the button that opens the modal
+    var btn = document.getElementById("buttonOpret");
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
     modal.style.display = "none";
-  }
-}
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        }
+    }
+ }
+
+
 
 var slider = document.getElementById("slider");
 
 var output = document.getElementById("afstand");
 
-output.innerHTML = slider.value; // Display the default slider value
+output.innerHTML = slider.value + " km"; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = this.value;
+  output.innerHTML = slider.value + " km";
+  console.log(slider.value);
 }
